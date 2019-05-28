77205d4721130762eebca256902fcf1f28997d76
Rodrigo Magalhaes <magalhaes.rodrigos@gmail.com>
2019-05-28 18:37:52 -0300

    teste localização

--- app.js
+++ app.js
@@ -41,6 +41,7 @@ app.post('/webhook', (req, res) => {
             if (entry.messaging) {
                 // Gets the body of the webhook event
                 let webhook_event = entry.messaging[0];
+                console.log(webhook_event);
 
                 // Get the sender PSID
                 let sender_psid = webhook_event.sender.id;
